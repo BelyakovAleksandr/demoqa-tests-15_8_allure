50c9295986b08650808c31768c48b6e644d9eaba
BelyakovAleksandr <113695437+BelyakovAleksandr@users.noreply.github.com>
2022-11-27 19:32:14 +0400

    demoqa-tests-15_8_allure

---
+++ src/test/java/tests/AnnotatedTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import steps.WebSteps;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedTest {
+
+    private static final String REPOSITORY = "KaNoNiR-FuN/qa_guru_15_10_hw";
+    private static final String ISSUE = "Test Issue";
+
+    @Test
+    public void findGitIssueTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.clickIssues();
+        steps.checkIssue(ISSUE);
+    }
+}
